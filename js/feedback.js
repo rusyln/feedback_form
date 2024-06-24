de2a3c3f134df8b57ba86c7b2793dcfae9934a4e
Rus <naritorusyl@gmail.com>
2024-06-24 10:21:07 +0800

    fix js

--- js/feedback.js
+++ js/feedback.js
@@ -1,7 +1,14 @@
 (function ($, Drupal) {
     Drupal.behaviors.feedbackForm = {
       attach: function (context, settings) {
+        console.log('Feedback Form behavior attached.');
+  
+        // Check if #feedback-tab element is found
+        console.log($('#feedback-tab', context));
+  
+        // Toggle #feedback-form on click
         $('#feedback-tab', context).click(function () {
+          console.log('Feedback Tab clicked.');
           $('#feedback-form').toggle();
         });
       }
